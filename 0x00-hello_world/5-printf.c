1cd10208ea12da0a3943ff567af79a09604f13af
my name <myemail@gmail.com>
2023-03-21 12:20:30 -0700

    task 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-* main - Write a C program that prints exactly with proper grammar,
-* but the outcome is a piece of art,.
+* main - A C program that prints exactly with proper grammar
 * Return: 0 (greati)
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,"\n)
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }

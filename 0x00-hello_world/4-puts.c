5aaf29d8a26f3ec55d3ae9e9c8c21f368b0757f7
my name <myemail@gmail.com>
2023-03-21 11:53:04 -0700

    task 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - An example of how to use the cmd puts
+ * Return: 0(great)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
